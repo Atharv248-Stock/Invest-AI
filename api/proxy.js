33d6a0feae4237338b0d8dfac8cb347e0617acb1
Atharv248-Stock <sancompounding@gmail.com>
2026-04-08 18:25:39 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -194,7 +194,9 @@ app.use((req, res, next) => {
 }
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-admin-secret');
+  if (origin && allowed.includes(origin)) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+}
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
